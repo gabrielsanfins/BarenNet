1ea49272ab0ee0fc7fe58f9eb44ee5fa3e4d2185
gabrielsanfins <gabrielsanfins@gmail.com>
2022-07-05 14:01:55 -0300

    First trial with laminar flow generated data was successfull

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,15 +8,16 @@ def Create_Similarity_Model(n_nonsimilar, n_similar):
     '''
     inputs = tf.keras.Input(shape = (n_nonsimilar +  n_similar,))
 
-    # build the exponential layer to represent the inputs of our Phi_1 function
     similar_parameters = tf.keras.layers.Lambda(lambda x : x[:, n_nonsimilar:])(inputs)
     nonsimilar_parameters = [0 for i in range(n_nonsimilar)]
     Phi1_lists_nonsimilar = [0 for i in range(n_nonsimilar)]
     Phi1_lists_similar = [0 for i in range(n_nonsimilar)]
     Phi1_input_list = [0 for i in range(n_nonsimilar)]
+
+    # build the exponential layer to represent the inputs of our Phi_1 function
     for i in range(n_nonsimilar):
         nonsimilar_parameters[i] = tf.keras.layers.Lambda(lambda x : x[:,i:i+1])(inputs)
-        Phi1_lists_nonsimilar[i] = tf.keras.layers.Dense(1, activation = 'exponential', use_bias = False)(similar_parameters)
+        Phi1_lists_nonsimilar[i] = tf.keras.layers.Dense(1, activation = 'exponential', use_bias = False, name = 'similarity_layer_' + str(i+1))(similar_parameters)
         Phi1_lists_similar[i] = tf.keras.layers.Lambda(lambda x : tf.exp(x))(nonsimilar_parameters[i])
         Phi1_input_list[i] = tf.keras.layers.Multiply()([Phi1_lists_nonsimilar[i], Phi1_lists_similar[i]])
     Phi1_input = tf.keras.layers.Concatenate()(Phi1_input_list)
@@ -28,11 +29,10 @@ def Create_Similarity_Model(n_nonsimilar, n_similar):
     phi1 = tf.keras.layers.Dense(1, activation = 'relu')(dense3)
 
     # build the layer for the multiplication of the similar parameters
-    multiplication_layer = tf.keras.layers.Dense(1, activation = 'exponential', use_bias = False)(similar_parameters)
+    multiplication_layer = tf.keras.layers.Dense(1, activation = 'exponential', use_bias = False, name = 'multiplication_layer')(similar_parameters)
 
     outputs = tf.keras.layers.Multiply()([phi1, multiplication_layer])
 
     model = tf.keras.Model(inputs = inputs, outputs = outputs)
-    model.summary()
 
     return model
