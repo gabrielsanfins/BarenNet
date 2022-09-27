f64d44a18eb983225c389581a6d58fb7d12b2516
gabrielsanfins <gabrielsanfins@gmail.com>
2022-09-27 11:30:55 -0300

    Danilo changed the code for the generation of the BarenNet, also reducing instability

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -7,35 +7,42 @@ def Create_Similarity_Model(n_nonsimilar, n_similar):
     Function that creates a neural network to discover incomplete similarities in the last n_similar paramaters of a dataset
     
     '''
+
     inputs = tf.keras.Input(shape = (n_nonsimilar +  n_similar,))
 
     similar_parameters = tf.keras.layers.Lambda(lambda x : x[:, n_nonsimilar:])(inputs)
-    nonsimilar_parameters = [0 for i in range(n_nonsimilar)]
-    Phi1_lists_nonsimilar = [0 for i in range(n_nonsimilar)]
-    Phi1_lists_similar = [0 for i in range(n_nonsimilar)]
-    Phi1_input_list = [0 for i in range(n_nonsimilar)]
+    nonsimilar_parameters = tf.keras.layers.Lambda(lambda x : x[:, :n_nonsimilar])(inputs)
+    Phi1_lists_nonsimilar = tf.keras.layers.Dense(n_nonsimilar, activation = 'exponential', use_bias = False, name = 'similarity_layer')(similar_parameters)
+    Phi1_lists_similar = tf.keras.layers.Lambda(lambda x : tf.exp(x))(nonsimilar_parameters)
+    Phi1_input = tf.keras.layers.Multiply()([Phi1_lists_nonsimilar, Phi1_lists_similar])
 
-    # build the exponential layer to represent the inputs of our Phi_1 function
-    for i in range(n_nonsimilar):
-        nonsimilar_parameters[i] = tf.keras.layers.Lambda(lambda x : x[:,i:i+1])(inputs)
-        Phi1_lists_nonsimilar[i] = tf.keras.layers.Dense(1, activation = 'exponential', use_bias = False, name = 'similarity_layer_' + str(i+1))(similar_parameters)
-        Phi1_lists_similar[i] = tf.keras.layers.Lambda(lambda x : tf.exp(x))(nonsimilar_parameters[i])
-        Phi1_input_list[i] = tf.keras.layers.Multiply()([Phi1_lists_nonsimilar[i], Phi1_lists_similar[i]])
-    if n_nonsimilar == 1:
-        Phi1_input = Phi1_input_list[0]
-    else:
-        Phi1_input = tf.keras.layers.Concatenate()(Phi1_input_list)
+    # # build the dense layers in order to fit our similarity function
+    # dense1 = tf.keras.layers.Dense(256, activation = 'gelu')(Phi1_input)
+    # bn1 = tf.keras.layers.BatchNormalization()(dense1)
+    # dense2 = tf.keras.layers.Dense(128, activation = 'gelu')(bn1)
+    # bn2 = tf.keras.layers.BatchNormalization()(dense2)
+    # dense3 = tf.keras.layers.Dense(64, activation = 'gelu')(bn2)
+    # bn3 = tf.keras.layers.BatchNormalization()(dense3)
+    # dense4 = tf.keras.layers.Dense(32, activation = 'gelu')(bn3)
+    # bn4 = tf.keras.layers.BatchNormalization()(dense4)
+    # dense5 = tf.keras.layers.Dense(16, activation = 'gelu')(bn4)
+    # bn5 = tf.keras.layers.BatchNormalization()(dense5)
+    # phi1 = tf.keras.layers.Dense(1, activation = None)(bn5)
 
-    # build the dense layers in order to fit our similarity function
-    dense1 = tf.keras.layers.Dense(128, activation = 'relu')(Phi1_input)
-    dense2 = tf.keras.layers.Dense(64, activation = 'relu')(dense1)
-    dense3 = tf.keras.layers.Dense(32, activation = 'relu')(dense2)
-    phi1 = tf.keras.layers.Dense(1, activation = 'relu')(dense3)
+    # Without batch normalization
+
+    dense1 = tf.keras.layers.Dense(256, activation = 'gelu')(Phi1_input)
+    dense2 = tf.keras.layers.Dense(128, activation = 'gelu')(dense1)
+    dense3 = tf.keras.layers.Dense(64, activation = 'gelu')(dense2)
+    dense4 = tf.keras.layers.Dense(32, activation = 'gelu')(dense3)
+    dense5 = tf.keras.layers.Dense(16, activation = 'gelu')(dense4)
+    phi1 = tf.keras.layers.Dense(1, activation = None)(dense5)
+
 
     # build the layer for the multiplication of the similar parameters
-    multiplication_layer = tf.keras.layers.Dense(1, activation = 'exponential', use_bias = False, name = 'multiplication_layer')(similar_parameters)
+    multiplication_layer = tf.keras.layers.Dense(1, activation = None, use_bias = False, name = 'multiplication_layer')(similar_parameters)
 
-    outputs = tf.keras.layers.Multiply()([phi1, multiplication_layer])
+    outputs = tf.keras.layers.Add()([phi1, multiplication_layer])
 
     model = tf.keras.Model(inputs = inputs, outputs = outputs)
 
