beec2b5e299d7a899ce3053c2d83f3f416bef768
gabrielsanfins <gabrielsanfins@gmail.com>
2023-07-25 19:37:39 -0300

    Adds two methods to the model class.
    They both are related to finding incomplete similarity through the
    barennet. The hidden method just saves a dictionary of the similarity
    exponents found by the neural network.

--- src/similarity_model/similarity_model.py
+++ src/similarity_model/similarity_model.py
@@ -1,5 +1,5 @@
 from typing import List, Dict
-import tensorflow as tf
+import numpy as np
 
 from .group_calculations import (
     find_buckingham_group_exponents_from_construction_dict)
@@ -179,3 +179,107 @@ class SimilarityModel:
                           "} ")
 
         return group_str
+
+    def find_incomplete_similarity(self,
+                                   dense_activation: str = "relu",
+                                   loss: str = "mean_squared_error",
+                                   optimizer: str = "adam",
+                                   n_tries: int = 10,
+                                   n_epochs: int = 100,
+                                   tol: float = 1e-3) -> None:
+        """
+
+        Creates an instance of barennet according to the non-similar and
+        similar parameters passed when the model was first instanced. It will
+        try to fit the barennet n_tries times and will consider the incomplete
+        similarity to exist only if the fit has an error of less than tol.
+
+        dense_activation must be a tensorflow activation function which will be
+        used in the dense part of the NN.
+
+        loss must be a tensorflow loss function.
+        optimizer must be a tensorflow optimizer.
+
+        """
+        xtrain = self.df_log_x
+        ytrain = self.df_log_y
+        best_loss = 1000000
+
+        n = 1
+        while n < n_tries:
+            model = create_barennet(
+                n_nonsimilar=self.n_nonsimilar,
+                n_similar=self.n_similar,
+                dense_activation=dense_activation
+            )
+            model.compile(loss=loss, optimizer=optimizer)
+            model.fit(xtrain, ytrain, epochs=n_epochs, verbose=2)
+
+            current_loss = model.evaluate(xtrain, ytrain, verbose=0)
+
+            if current_loss < best_loss:
+                best_loss = current_loss
+
+            if current_loss < tol:
+                print("Incomplete Similarity Found!")
+                print("Error = " + current_loss)
+                self.barennet_model = model
+                self._save_incomplete_similarity_relation()
+                self.found_incomplete_similarity = True
+                return None
+
+            n += 1
+
+        self.found_incomplete_similarity = False
+
+        print("Incomplete Similarity was not found!")
+        print("However, the best error found was " +
+              str(np.round(best_loss, 3)) + ".")
+        print("If you feel this error is small enough, consider increasing"
+              "n_tries or n_epochs, or decreasing the tolerance.")
+
+        return None
+
+    def _save_incomplete_similarity_relation(self) -> None:
+        """
+
+        Saves the incomplete similarity relation found through barennet. In
+        order for this method to work, an incomplete similarity relation should
+        have been found with the method find_incomplete_similarity.
+
+        The exponents of the incomplete similarity relation will be saved in
+        the attribute self.incomplete_similarity_exponents_dict.
+
+        """
+        if not self.found_incomplete_similarity:
+            print("Incomplete similarity was not found. In order for an "
+                  "incomplete similarity relation to exist, we first need "
+                  "to sucessfully find it with the method "
+                  "find_incomplete_similarity.")
+            return None
+
+        exponents_dict = {}
+
+        for i in range(len(self.non_similar_params)):
+            exponents_dict[self.non_similar_params[i]] = {}
+            for j in range(len(self.similar_params)):
+                exponents_dict[self.non_similar_params[i]][
+                    self.similar_params[j]
+                ] = (
+                    self.barennet_model.get_layer(
+                        'similarity_layer').weights[0][j][i].numpy()
+                    )
+
+        for i in range(len(self.non_dimensional_qoi)):
+            exponents_dict[self.non_dimensional_qoi[i]] = {}
+            for j in range(len(self.similar_params)):
+                exponents_dict[self.non_dimensional_qoi[i]][
+                    self.similar_params[j]
+                ] = (
+                    self.barennet_model.get_layer(
+                        'multiplication_layer').weights[0][j][i].numpy()
+                    )
+
+        self.incomplete_similarity_exponents_dict = exponents_dict
+
+        return None
