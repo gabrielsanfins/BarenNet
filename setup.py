7004779c8bcdb4336493314d4cc8868ba98e51b6
gabrielsanfins <gabrielsanfins@gmail.com>
2023-12-01 13:45:36 -0300

    Updates the requiremets of the project in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     name='barennet',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'numpy>=1.21.4',
+        'pandas>=1.3.4',
+        'tensorflow>=2.14.0',
+        'tensorflow_cpu>=2.9.1'
+    ],
     author="Gabriel Sanfins",
     author_email="gabrielsanfins@gmail.com",
     description="Public repository for research in automatic incomplete similarity discovery",
-    install_requires=[
-        "numpy",
-        "pandas",
-        "tensorflow_cpu"
-    ]
 )
